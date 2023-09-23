f9f4d99a4e5fcb6ff822cd5a4c5634bc00bef93d
Lorenz2202 <98314332+Lorenz2202@users.noreply.github.com>
2023-09-23 21:27:18 +0800

    Delete QA Deliverables/Bulldogs Book Express directory

--- QA Deliverables/Bulldogs Book Express/Redirect_into_admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("settings.php");
-session_start();
-mysqli_query($conn,"DELETE FROM dummy");
-header("location:admin.php");
-?>
--- QA Deliverables/Bulldogs Book Express/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0);
-$servername = "localhost";
-$username = "id21290364_angeleslr";
-$password = "Lorenz12345!";
-$dbname = "id21290364_library";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
