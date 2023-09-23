8130c4b8b591b3d6e40710eac169b8fe7d4994b2
Lorenz2202 <98314332+Lorenz2202@users.noreply.github.com>
2023-09-23 21:24:12 +0800

    Add files via upload

---
+++ QA Deliverables/Bulldogs Book Express/settings.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(0);
+$servername = "localhost";
+$username = "id21290364_angeleslr";
+$password = "Lorenz12345!";
+$dbname = "id21290364_library";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
